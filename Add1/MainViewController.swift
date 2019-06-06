da5bba4c55fb73bcf870e10e090f78cea14d529b
Greg Auger <gpauger@gmail.com>
2019-06-06 11:27:33 -0700

    Initial Commit

---
+++ Add1/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Add1
+//
+//  Created by Greg Auger on 6/6/19.
+//  Copyright © 2019 gpauger. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
